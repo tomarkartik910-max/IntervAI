61fc574c9a7290e8a102c89f093d9c8dfb8b1994
Kartik Tomar <tomarkartik910@gmail.com>
2026-08-22 02:05:51 +0530

    Add AuthModel component and integrate with Navbar for user authentication

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
 
 const Home = () => {
   return (
-    <div>
-      Home page
+    <div className='min-h-screen bg-[#f3f3f3] flex flex-col'>
+      <Navbar/>
     </div>
   )
 }
