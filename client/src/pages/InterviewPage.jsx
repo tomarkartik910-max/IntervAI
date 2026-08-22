75781009f80a39d7ec3d62cc8a069f0a292263b8
Kartik Tomar <tomarkartik910@gmail.com>
2026-08-22 22:15:45 +0530

    setup openrouter and make api to analyseresume and created the concerning frontend page

--- client/src/pages/InterviewPage.jsx
+++ client/src/pages/InterviewPage.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from 'react'
+import Step1SetUp from '../components/Step1SetUp'
+import Step2Interview from '../components/Step2Interview'
+import Step3Report from '../components/Step3Report'
 
 function InterviewPage() {
     const [step,setStep] = useState(1)
--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ import cors from "cors"
 import cookieParser from "cookie-parser"
 import authRouter from "./routes/auth.route.js"
 import userRouter from "./routes/user.route.js"
+import interviewRouter from "./routes/interview.route.js"
 
 const app = express()
 
@@ -19,6 +20,7 @@ app.use(cookieParser())
 
 app.use("/api/auth",authRouter)
 app.use("/api/user",userRouter)
+app.use("/api/interview",interviewRouter)
 
 const PORT = process.env.PORT || 6000
 app.listen(PORT,()=>{
