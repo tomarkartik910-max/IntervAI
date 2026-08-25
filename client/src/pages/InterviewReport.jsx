981300afa18a90fa1fa98e6b82689fc2d1804a7e
Kartik Tomar <tomarkartik910@gmail.com>
2026-08-25 23:22:34 +0530

    completed report page and download pdf option

--- client/src/pages/InterviewReport.jsx
+++ client/src/pages/InterviewReport.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from "axios"
+import { ServerUrl } from '../App'
+import { useParams } from 'react-router-dom'
+import Step3Report from '../components/Step3Report'
 
 function InterviewReport() {
-  return (
-    <div>
-      
-    </div>
-  )
+
+  const {id} = useParams()
+  const [report,setReport] = useState(null)
+
+  useEffect(()=>{
+    const fetchReport = async () => {
+      try {
+        const result = await axios.get(ServerUrl+"/api/interview/report/"+id,{withCredentials:true})
+
+        setReport(result.data)
+      } catch (error) {
+        console.log(error);
+        
+      }
+    }
+    fetchReport();
+  },[])
+
+  if(!report){
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <p className="text-gray-500 text-lg">
+          Loading Report...
+        </p>
+      </div>
+    );
+  }
+
+  return <Step3Report report={report}/>
 }
 
 export default InterviewReport
