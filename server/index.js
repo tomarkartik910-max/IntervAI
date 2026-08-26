e35b60a69419d167aa839a6c08c1c03e367680fb
Kartik Tomar <tomarkartik910@gmail.com>
2026-08-26 15:01:36 +0530

    completed payment setup

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ import cookieParser from "cookie-parser"
 import authRouter from "./routes/auth.route.js"
 import userRouter from "./routes/user.route.js"
 import interviewRouter from "./routes/interview.route.js"
+import paymentRouter from "./routes/payment.route.js"
 
 const app = express()
 
@@ -21,6 +22,7 @@ app.use(cookieParser())
 app.use("/api/auth",authRouter)
 app.use("/api/user",userRouter)
 app.use("/api/interview",interviewRouter)
+app.use("/api/payment",paymentRouter)
 
 const PORT = process.env.PORT || 6000
 app.listen(PORT,()=>{
